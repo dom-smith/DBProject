2ae59d77fd6908e004050f27a74ca936894b46be
DG-1337 <devgopal745@gmail.com>
2025-03-31 12:23:58 -0400

    Added css styling

--- website/src/components/Signup/Signup.jsx
+++ website/src/components/Signup/Signup.jsx
@@ -5,34 +5,36 @@ import "./Signup.css";
 const Signup = () => {
     return (
         <div className="container">
-            <div className="header"></div>
+            <div className="header">
                 <div className="text">Sign Up</div>
                 <div className="underline"></div>
+            </div>
             <div className="inputs">
                 <div className="input">
                     <i className="material-icons">person</i>
-                    <input type="text" />
+                    <input type="text" placeholder="Name"/>
                 </div>
                 <div className="input">
                     <i className="material-icons">mail</i>
-                    <input type="email" />
+                    <input type="email" placeholder="Email"/>
                 </div>
                 <div className="input">
                     <i className="material-icons">key</i>
-                    <input type="password" />
+                    <input type="password" placeholder="Password"/>
                 </div>
                 <div className="input">
                     <i className="material-icons">badge</i>
-                    <input type="password" />
+                    <input type="text" placeholder="User Type"/>
                 </div>
                 <div className="input">
                     <i className="material-icons">school</i>
-                    <input type="password" />
+                    <input type="text" placeholder="University"/>
                 </div>
+                <button className="sign-up">Sign Up</button>
             </div>
-            <div className="forgot-password">Forgot Password?</div>
-            <div className="submit-container">
-                <div className="submit">Already Have an Account?</div>
+            <div className="forgot-login-container">
+                <div className="forgot-password">Forgot Password?</div>
+                <div className="login-page">Already Have an Account?</div>
             </div>
         </div>
     )
