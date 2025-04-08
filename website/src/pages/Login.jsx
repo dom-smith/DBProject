99f0881b6804458c08a57738b5810752a5b52555
DG-1337 <devgopal745@gmail.com>
2025-04-08 10:31:35 -0400

    handled user login through supabase

--- website/src/pages/Login.jsx
+++ website/src/pages/Login.jsx
@@ -1,44 +1,62 @@
 import supabase from "../utils/SupabaseClient";
 import { useEffect, useState } from "react";
 import "./Signup-Login.css"; 
+import { useNavigate } from "react-router-dom";
 
 
 const Login = () => {
-    const[name, setName] = useState('');
-    const [method, setMethod] = useState(''); 
+    const[email, setEmail] = useState('');
     const [password, setPassword] = useState(''); 
-    const [role, setRole] = useState(''); 
     const [formError, setFormError] = useState(null); 
+    const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault(); 
+        setFormError(null); 
 
+        if(!email || !password) {
+            setFormError("Please input all fields");
+            return; 
+        }
+
+        // sign in the user
+        const {data: userData, error: userError} = await supabase.auth.signInWithPassword({ email, password }); 
+
+        if(userError) {
+            console.log("Auth Error: ", userError);
+            setFormError(userError.error); 
+            return
+        }
+
+        console.log("Sign in successful", userData); 
+        navigate("/")
+        
     }
     return (
         <div className="container">
-        <div className="header">
-            <div className="text">Event Manager</div>
-            <div className="underline"></div>
-        </div>
-        <div className="inputs">
-            <div className="input">
-                <i className="material-icons">mail</i>
-                <input type="email" placeholder="Email"/>
-            </div>
-            <div className="input">
-                <i className="material-icons">key</i>
-                <input type="password" placeholder="Password"/>
-            </div>
-            <div className="input">
-                <i className="material-icons">school</i>
-                <input type="text" placeholder="University"/>
-            </div>
-            <button className="sign-up">Login</button>
-        </div>
-        <div className="forgot-login-container">
-            <div className="forgot-password"><a href="/">Forgot Password?</a></div>
-            <div className="login-page"><a href="/sign-up">Don't Have an Account?</a></div>
-        </div>
+            <form onSubmit={handleSubmit}>
+                <div className="header">
+                    <div className="text">Event Manager</div>
+                    <div className="underline"></div>
+                </div>
+
+                <div className="inputs">
+                        <div className="input">
+                            <i className="material-icons">mail</i>
+                            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                        <div className="input">
+                            <i className="material-icons">key</i>
+                            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                        </div>
+                        <button type="submit" className="sign-up">Login</button>
+                        <div className="forgot-login-container">
+                            <a href="/">Forgot Password?</a>
+                            <a href="/sign-up">Don't Have an Account?</a>
+                        </div>
+                </div>
+                {formError && <div className="error">{formError}</div>}
+            </form>
         </div>
     )
 }
